15d69c9abded8c18925956e6c837b129f61bec9d
FacuM <facumo.fm@gmail.com>
2018-10-21 00:34:34 +0000

    BoardConfigCommon: set the right CPU architecture.
    Change-Id: I85cabce834db9bbc2f633ac2b5510ebea522759f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,7 +38,7 @@ TARGET_NO_BOOTLOADER := true
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v7a
+TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
